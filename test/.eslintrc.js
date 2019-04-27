b22c9e0a95d0f72ff0ac81666fbd75f7a1077364
overlookmotel <theoverlookmotel@gmail.com>
2019-04-27 12:50:21 +0100

    Dev: Lint tests with self

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -1,14 +1,3 @@
 'use strict';
 
-module.exports = {
-	env: {
-		mocha: true
-	},
-	plugins: [
-		'chai-friendly'
-	],
-	rules: {
-		'no-unused-expressions': ['off'],
-		'chai-friendly/no-unused-expressions': ['error']
-	}
-};
+module.exports = require('../index');
